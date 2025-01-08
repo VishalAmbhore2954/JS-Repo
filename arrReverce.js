dd6f0b2289a18218a83e0fac6f1def85451c0d67
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-01-08 16:25:18 +0530

    Add

---
+++ arrReverce.js
@@ -0,0 +1,11 @@
+let arr = [10,20,30,40,50,60,70,80,90];
+
+//printing array as it is
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]); 
+}
+
+//printing reverce array 
+for(let i=arr.length;i>=0;i--){
+    console.log(arr[i]); 
+}
