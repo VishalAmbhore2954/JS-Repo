0c75247e2d81b7030e0e02f1a37f8e4a3d9e4e57
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-01-08 16:40:40 +0530

    Add

---
+++ operations.js
@@ -0,0 +1,12 @@
+let a = 20;
+let b = 10;
+
+let c = a+b;
+let d = a-b;
+let e = a*b;
+let f = a/b;
+
+console.log("Addition is "+c)
+console.log("Substraction is "+d)
+console.log("Multiplition is "+e)
+console.log("Division is "+f)
