a69c06434ecba744efa071a75409a15093d8c720
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-01-08 15:49:46 +0530

    Difference btwn var and let

---
+++ let&var.js
@@ -0,0 +1,10 @@
+var a = 10;
+// console.log(a);
+var a = 20;
+console.log(a); //We can redeclare variable with var
+
+let b = 10;
+console.log(b);
+
+//We can't redeclare variable using let 
+//let b = 20; Its showing error
